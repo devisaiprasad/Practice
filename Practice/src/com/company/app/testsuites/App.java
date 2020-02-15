c9e37b5f7b89a840178816a54a306fd81a5d6dd7
Chinni <Chinni@DESKTOP-M57K9LG>
2020-02-15 14:31:39 +0530

    twe

--- Practice/src/com/company/app/testsuites/App.java
+++ Practice/src/com/company/app/testsuites/App.java
@@ -5,16 +5,34 @@ import org.testng.annotations.Test;
 
 public class App 
 {
-   
+	@Test()
+    public void testMethodOne() {
+        System.out.println("Test method one");
+    }
+ 
+    @Test()
+    public void testMethodTwo() {
+        System.out.println("Test method two");
+    }
+ 
+    @Test()
+    public void testMethodThree() {
+        System.out.println("Test method three");
+    }
+ 
+    @Test()
+    public void testMethodFour() {
+        System.out.println("Test method Four");
+    }
     
-    @Test
+    @Test()
     public static void print( )
     {
         System.out.println( "Hello World!" );
     }
     
     
-    @Test
+    @Test()
     public static void print1( )
     {
         System.out.println( "Method Fail" );
